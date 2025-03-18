f8ebc386ba75b54927f37b21b246138ecef1174c
Sword <verigo.dmitry@gmail.com>
2025-03-18 17:52:41 +0300

    Changed main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from colorama import Fore, Style
 import datetime
 import statistics
 
-sells = [] # [[name, price, category], [name, price, category], ...]
+sells = [] # [[name, price, quantity, category], [name, price, quantity, category], ...]
 data = {}
 
 print(f"{Fore.YELLOW}Welcome to the sell tracker!{Fore.RESET}")
-print("Write a sell in the format" + Fore.CYAN + " name:price:category" + Fore.RESET, end='. ')
+print("Write a sell in the format" + Fore.CYAN + " name:price_for_one_unit:quantity:category" + Fore.RESET, end='. ')
 print("If you want to stop writing sells, type '" + Fore.CYAN + "stop" + Fore.RESET + "'.")
 
 # Main input
@@ -18,22 +18,16 @@ while True:
     if user_input == "stop":
         if not sells: print(Fore.RED + "You have not written any sell yet" + Fore.RESET) # If sells list is empty
         else: break
-    elif len(splited_input) == 3 and all(splited_input): # Check if input is valid
-        if splited_input[1].isnumeric() and float(splited_input[1]) > 0: # Check if price is a valid number
+    elif len(splited_input) == 4 and all(splited_input): # Check if input is valid
+        if splited_input[1].isnumeric() and float(splited_input[1]) > 0 and splited_input[2].isnumeric() and float(splited_input[2]) > 0: # Check if price is a valid number
             print(Fore.GREEN + f"Sell submitted ({splited_input[0]})" + Fore.RESET)
             sells.append(splited_input)
         else:
-            print(Fore.RED + "Your price is not a valid number. Your sell will not be submitted" + Fore.RESET)
+            print(Fore.RED + "Your price or quantity is not a valid number. Your sell will not be submitted" + Fore.RESET)
     else:
         print(Fore.RED + "Invalid format. Your sell will not be submitted" + Fore.RESET)
         continue
 
-# Overall sells
-print(Style.BRIGHT + Fore.MAGENTA + f"\nOVERALL SELLS" + Style.RESET_ALL, end="")
-print(f"""
-    ● Products: {', '.join([sell[0] for sell in sells])}
-    ● Overall price: {sum([float(sell[1]) for sell in sells])}
-    ● Average price: {round(statistics.mean([float(sell[1]) for sell in sells]), 2)}""" + Style.RESET_ALL)
 
 data["DATE"] = datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")
 data["OVERALL"] = {
@@ -43,29 +37,32 @@ data["OVERALL"] = {
 }
 data["BY_CATEGORIES"] = {}
 
+# Overall sells
+print(f"""Style.BRIGHT + Fore.MAGENTA + "\nOVERALL SELLS" + Style.RESET_ALL
+● Products: {', '.join(data["OVERALL"]["products"])}
+● Overall price: {data["OVERALL"]["overall_price"]}
+● Average price: {data["OVERALL"]["average_price"]}
+""")
+
 # Sells by categories
-print(Style.BRIGHT + Fore.MAGENTA + "\nSELLS BY CATEGORIES" + Style.RESET_ALL)
-
-
-for product, price, category in sells:
-    price = float(price)
+for product, price, quantity, category in sells:
+    price = float(price); quantity = int(quantity)
     if category not in data["BY_CATEGORIES"].keys():
-        data["BY_CATEGORIES"][category.lower()] = {
+        data["BY_CATEGORIES"][category] = {
             "products": [],
-            "overall_price": None,
-            "average_price": None
+            "overall_price": 0,
+            "average_price": 0
         }
     data["BY_CATEGORIES"][category]["products"].append(product)
-    if data["BY_CATEGORIES"][category]["overall_price"]: data["BY_CATEGORIES"][category]["overall_price"] += price
-    else: data["BY_CATEGORIES"][category]["overall_price"] = price
+    data["BY_CATEGORIES"][category]["overall_price"] += price * quantity
 
+print(Style.BRIGHT + Fore.MAGENTA + "\nSELLS BY CATEGORIES" + Style.RESET_ALL)
 for category in data["BY_CATEGORIES"].keys():
-    prices = [float(sell[1]) for sell in sells if sell[2] == category]
-    data["BY_CATEGORIES"][category]["average_price"] = round(statistics.mean(prices), 2)
+    data["BY_CATEGORIES"][category]["average_price"] = round(data["BY_CATEGORIES"][category]["overall_price"] / sum([int(sell[2]) for sell in sells if sell[3] == category]), 2)
     print(f"""Category: {Fore.YELLOW + category.upper() + Fore.RESET}
         ● Products: {', '.join(data["BY_CATEGORIES"][category]["products"])}
-        ● Overall price: {sum(prices)}
-        ● Average price: {round(statistics.mean(prices), 2)}""")
+        ● Overall price: {data["BY_CATEGORIES"][category]["overall_price"]}
+        ● Average price: {round(data["BY_CATEGORIES"][category]["overall_price"] / sum([int(sell[2]) for sell in sells if sell[3] == category]), 2)}""")
 
 
 date_time = datetime.datetime.now()
